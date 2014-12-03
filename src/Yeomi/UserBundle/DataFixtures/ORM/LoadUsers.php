abf75214eef4fad52a41e58bd192e808a7cb2d18
bindou <gabriel@henao.fr>
2014-12-04 00:07:10 +0100

    creation of Doctrine entity User and Role, adaptation of entity User to core interface UserInterface, for now we have ROLE_USER and ROLE_ADMIN

---
+++ src/Yeomi/UserBundle/DataFixtures/ORM/LoadUsers.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bindou
+ * Date: 03/12/2014
+ * Time: 23:13
+ */
+
+namespace Yeomi\UserBundle\DataFixtures\ORM;
+
+
+use Doctrine\Common\DataFixtures\Doctrine;
+use Doctrine\Common\DataFixtures\FixtureInterface;
+use Doctrine\Common\Persistence\ObjectManager;
+use Yeomi\UserBundle\Entity\Role;
+use Yeomi\UserBundle\Entity\User;
+
+class LoadUsers implements FixtureInterface {
+
+    /**
+     * Load data fixtures with the passed EntityManager
+     *
+     * @param Doctrine\Common\Persistence\ObjectManager $manager
+     */
+    function load(ObjectManager $manager)
+    {
+        $users = array(
+            'bindou' => array(
+                'email' => 'bindou@bin.fr',
+                'password' => '1234',
+            ),
+            'bumbo' => array(
+                'email' => 'boumboum@bumb.fr',
+                'password' => '1234',
+            ),
+            'damour' => array(
+                'email' => 'damour@binbumb.fr',
+                'password' => '1234',
+            )
+        );
+
+
+        $roleA = new Role();
+        $roleA->setName('Authenticated User')
+            ->setSlug('ROLE_USER');
+        $roleB = new Role();
+        $roleB->setName('Premium User')
+            ->setSlug('ROLE_USER_PREMIUM');
+
+        foreach ($users as $username => $userData) {
+            $user = new User();
+
+
+            $user->setUsername($username)
+                ->setEmail($userData["email"])
+                ->setPassword($userData["password"])
+                ->addRole($roleA)
+                ->addRole($roleB);
+
+            $manager->persist($user);
+        }
+
+        $manager->flush();
+
+    }
+}
---
+++ src/Yeomi/UserBundle/Entity/User.php
@@ -0,0 +1,280 @@
+<?php
+
+namespace Yeomi\UserBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Doctrine\Common\Collections\ArrayCollection;
+
+/**
+ * User
+ *
+ * @ORM\Table("user")
+ * @ORM\Entity(repositoryClass="Yeomi\UserBundle\Entity\UserRepository")
+ */
+class User implements UserInterface
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var ArrayCollection
+     *
+     * @ORM\ManyToMany(targetEntity="Yeomi\UserBundle\Entity\Role", cascade={"persist"})
+     */
+    private $roles;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="username", type="string", length=255, unique=true)
+     */
+    private $username;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     */
+    private $password;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="salt", type="string", length=255, nullable=true)
+     */
+    private $salt;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     */
+    private $email;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created", type="datetime")
+     */
+    private $created;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="last_login", type="datetime", nullable=true)
+     */
+    private $lastLogin;
+
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->roles = new ArrayCollection();
+        $this->created = new \DateTime();
+    }
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set username
+     *
+     * @param string $username
+     * @return User
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get username
+     *
+     * @return string 
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     * @return User
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string 
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set salt
+     *
+     * @param string $salt
+     * @return User
+     */
+    public function setSalt($salt)
+    {
+        $this->salt = $salt;
+
+        return $this;
+    }
+
+    /**
+     * Get salt
+     *
+     * @return string 
+     */
+    public function getSalt()
+    {
+        return $this->salt;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     * @return User
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string 
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set created
+     *
+     * @param \DateTime $created
+     * @return User
+     */
+    public function setCreated($created)
+    {
+        $this->created = $created;
+
+        return $this;
+    }
+
+    /**
+     * Get created
+     *
+     * @return \DateTime 
+     */
+    public function getCreated()
+    {
+        return $this->created;
+    }
+
+    /**
+     * Set lastLogin
+     *
+     * @param \DateTime $lastLogin
+     * @return User
+     */
+    public function setLastLogin($lastLogin)
+    {
+        $this->lastLogin = $lastLogin;
+
+        return $this;
+    }
+
+    /**
+     * Get lastLogin
+     *
+     * @return \DateTime 
+     */
+    public function getLastLogin()
+    {
+        return $this->lastLogin;
+    }
+
+    /**
+     * Add roles
+     *
+     * @param \Yeomi\UserBundle\Entity\Role $roles
+     * @return User
+     */
+    public function addRole(\Yeomi\UserBundle\Entity\Role $roles)
+    {
+        $this->roles[] = $roles;
+
+        return $this;
+    }
+
+    /**
+     * Remove roles
+     *
+     * @param \Yeomi\UserBundle\Entity\Role $roles
+     */
+    public function removeRole(\Yeomi\UserBundle\Entity\Role $roles)
+    {
+        $this->roles->removeElement($roles);
+    }
+
+    /**
+     * Get roles
+     *
+     * @return \Doctrine\Common\Collections\Collection 
+     */
+    public function getRoles()
+    {
+        $roles = array();
+
+        foreach ($this->roles as $role) {
+            $roles []= $role->getSlug();
+        }
+        return $roles;
+    }
+
+    /**
+     * Removes sensitive data from the user.
+     *
+     * This is important if, at any given point, sensitive information like
+     * the plain-text password is stored on this object.
+     */
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+}
