e0e1ca8b559aec624c555b3b12b2c3b3c4b9771a
Gabriel <gabriel@henao.fr>
2015-01-31 22:15:20 +0100

    gregware/ImageBundle

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Yeomi\PostBundle\YeomiPostBundle(),
             new Yeomi\AdminBundle\YeomiAdminBundle(),
             new Yeomi\CMSBundle\YeomiCMSBundle(),
+            new Gregwar\ImageBundle\GregwarImageBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
